d93335c92476039b9307ce34e11da01001ee3395
Refat E Ferdous <reafat.fardous@welldev.io>
2023-12-26 17:17:24 +0600

    refactor the code

--- WeatherApp/Utility/ConstantKeys.swift
+++ WeatherApp/Utility/ConstantKeys.swift
@@ -13,9 +13,10 @@ class ConstantKeys{
     var latitude : Double = 0.0
     var longitude : Double = 0.0
     var cityName : String = ""
-    static var BASE_URL : String = "https://api.openweathermap.org/data/2.5"
-    static var APP_ID : String = "821f770339f2c200486a644b17bf970a"
-    static var IMAGE_URL : String = "https://openweathermap.org/img/w/"
-    static var IMAGE_EXTENSION : String = ".png"
+
+    static let BASE_URL : String = "https://api.openweathermap.org/data/2.5"
+    static let APP_ID : String = "821f770339f2c200486a644b17bf970a"
+    static let IMAGE_URL : String = "https://openweathermap.org/img/w/"
+    static let IMAGE_EXTENSION : String = ".png"
     
 }
